0c28f708e2413219286f8acd19b4a6bd812c723e
Juliette Lelchuk <juliettelelchuk@mindsetconsulting.com>
2023-08-08 15:33:59 -0500

    Backend data populating EmployeeList

--- adt_ui5/webapp/controller/EmployeeList.controller.js
+++ adt_ui5/webapp/controller/EmployeeList.controller.js
@@ -1,12 +1,13 @@
 sap.ui.define([
     "sap/ui/core/mvc/Controller",
+    "sap/ui/core/UIComponent",
     "sap/ui/model/json/JSONModel"
 
 ],
     /**
      * @param {typeof sap.ui.core.mvc.Controller} Controller
      */
-    function (Controller, JSONModel) {
+    function (Controller, UIComponent, JSONModel) {
         "use strict";
         var controller, component;
         return Controller.extend("mindset.adt.ui5.adtui5.controller.EmployeeList", {
@@ -17,56 +18,65 @@ sap.ui.define([
 
                 component = this.getOwnerComponent();
 
-                var oData = {
-                    Employees: [
-                        {
-                            Id: "1001",
-                            Name: "John Doe",
-                            Department: "Sales",
-                            StartDate: "2023-08-01T00:00:00Z",
-                            Role: "Manager",
-                            Address: "123 Main St",
-                            CityState: "Anytown, USA",
-                            ZipCode: "12345",
-                            Email: "john.doe@example.com",
-                            PhoneNumber: "555-123-4567",
-                            Photo: "url/to/photo.jpg"
-                        },
-                        {
-                            Id: "1002",
-                            Name: "Jane Smith",
-                            Department: "Marketing",
-                            StartDate: "2023-08-01T00:00:00Z",
-                            Role: "Marketing Specialist",
-                            Address: "456 Oak Ave",
-                            CityState: "Anothercity, USA",
-                            ZipCode: "67890",
-                            Email: "jane.smith@example.com",
-                            PhoneNumber: "555-987-6543",
-                            Photo: "url/to/photo2.jpg"
-                        },
-                        {
-                            Id: "1003",
-                            Name: "Bob Johnson",
-                            Department: "IT",
-                            StartDate: "2023-08-01T00:00:00Z",
-                            Role: "Software Engineer",
-                            Address: "789 Elm St",
-                            CityState: "Techcity, USA",
-                            ZipCode: "54321",
-                            Email: "bob.johnson@example.com",
-                            PhoneNumber: "555-555-5555",
-                            Photo: "url/to/photo3.jpg"
-                        }
-                    ]
-                };
+                // var oData = {
+                //     Employees: [
+                //         {
+                //             Id: "1001",
+                //             Name: "John Doe",
+                //             Department: "Sales",
+                //             StartDate: "2023-08-01T00:00:00Z",
+                //             Role: "Manager",
+                //             Address: "123 Main St",
+                //             CityState: "Anytown, USA",
+                //             ZipCode: "12345",
+                //             Email: "john.doe@example.com",
+                //             PhoneNumber: "555-123-4567",
+                //             Photo: "url/to/photo.jpg"
+                //         },
+                //         {
+                //             Id: "1002",
+                //             Name: "Jane Smith",
+                //             Department: "Marketing",
+                //             StartDate: "2023-08-01T00:00:00Z",
+                //             Role: "Marketing Specialist",
+                //             Address: "456 Oak Ave",
+                //             CityState: "Anothercity, USA",
+                //             ZipCode: "67890",
+                //             Email: "jane.smith@example.com",
+                //             PhoneNumber: "555-987-6543",
+                //             Photo: "url/to/photo2.jpg"
+                //         },
+                //         {
+                //             Id: "1003",
+                //             Name: "Bob Johnson",
+                //             Department: "IT",
+                //             StartDate: "2023-08-01T00:00:00Z",
+                //             Role: "Software Engineer",
+                //             Address: "789 Elm St",
+                //             CityState: "Techcity, USA",
+                //             ZipCode: "54321",
+                //             Email: "bob.johnson@example.com",
+                //             PhoneNumber: "555-555-5555",
+                //             Photo: "url/to/photo3.jpg"
+                //         }
+                //     ]
+                // };
 
-                var oModel = new JSONModel(oData);
-                this.getView().setModel(oModel);
+                // var oModel = new JSONModel(oData);
+                // this.getView().setModel(oModel);
             },
             onPress: function (oEvent) {
                 // The source is the list item that got pressed
                 this._showObject(oEvent.getSource());
-            }
+            },
+            _showObject: function (oItem) {
+                this.getRouter().navTo("object", {
+                    objectId: oItem.getBindingContext().getPath().substring("/SkillSet".length)
+                });
+            },
+
+            getRouter: function () {
+                return UIComponent.getRouterFor(this);
+            },
         });
     });
