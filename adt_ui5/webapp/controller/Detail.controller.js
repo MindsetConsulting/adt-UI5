5468b1e18056fbe6d53fdd11eb4e0a46512bd6af
Juliette Lelchuk <juliettelelchuk@mindsetconsulting.com>
2023-08-09 14:03:57 -0500

    Detail page displays hardcoded data

--- adt_ui5/webapp/controller/Detail.controller.js
+++ adt_ui5/webapp/controller/Detail.controller.js
@@ -1,12 +1,14 @@
 sap.ui.define([
     "sap/ui/core/mvc/Controller",
-    "sap/ui/model/json/JSONModel"
+    "sap/ui/model/json/JSONModel",
+    "sap/ui/core/UIComponent",
+
 
 ],
     /**
      * @param {typeof sap.ui.core.mvc.Controller} Controller
      */
-    function (Controller, JSONModel) {
+    function (Controller, JSONModel, UIComponent) {
         "use strict";
         var controller, component;
         return Controller.extend("mindset.adt.ui5.adtui5.controller.Detail", {
@@ -16,55 +18,19 @@ sap.ui.define([
                 // this.getView().setModel(ViewModel);
 
                 component = this.getOwnerComponent();
+                
                 this.getRouter().getRoute("Detail").attachPatternMatched(this._onObjectMatched, this);
+                
 
-
-                var oData = {
-                    Employees: [
-                        {
-                            Id: "1001",
-                            Name: "John Doe",
-                            Department: "Sales",
-                            StartDate: "2023-08-01T00:00:00Z",
-                            Role: "Manager",
-                            Address: "123 Main St",
-                            CityState: "Anytown, USA",
-                            ZipCode: "12345",
-                            Email: "john.doe@example.com",
-                            PhoneNumber: "555-123-4567",
-                            Photo: "url/to/photo.jpg"
-                        },
-                        {
-                            Id: "1002",
-                            Name: "Jane Smith",
-                            Department: "Marketing",
-                            StartDate: "2023-08-01T00:00:00Z",
-                            Role: "Marketing Specialist",
-                            Address: "456 Oak Ave",
-                            CityState: "Anothercity, USA",
-                            ZipCode: "67890",
-                            Email: "jane.smith@example.com",
-                            PhoneNumber: "555-987-6543",
-                            Photo: "url/to/photo2.jpg"
-                        },
-                        {
-                            Id: "1003",
-                            Name: "Bob Johnson",
-                            Department: "IT",
-                            StartDate: "2023-08-01T00:00:00Z",
-                            Role: "Software Engineer",
-                            Address: "789 Elm St",
-                            CityState: "Techcity, USA",
-                            ZipCode: "54321",
-                            Email: "bob.johnson@example.com",
-                            PhoneNumber: "555-555-5555",
-                            Photo: "url/to/photo3.jpg"
-                        }
-                    ]
-                };
-
-                var oModel = new JSONModel(oData);
-                this.getView().setModel(oModel);
+                // var oModel = new JSONModel(oData);
+                // this.getView().setModel(oModel);
+            }, 
+            getRouter: function () {
+                return UIComponent.getRouterFor(this);
+            },
+            _onObjectMatched: function (oEvent) {
+                var sObjectId = oEvent.getParameter("arguments").employeeId;
+                // this._bindView("/Employee" + sObjectId);
             }
         });
     });
--- adt_ui5/webapp/controller/EmployeeList.controller.js
+++ adt_ui5/webapp/controller/EmployeeList.controller.js
@@ -18,50 +18,6 @@ sap.ui.define([
 
                 component = this.getOwnerComponent();
 
-                // var oData = {
-                //     Employees: [
-                //         {
-                //             Id: "1001",
-                //             Name: "John Doe",
-                //             Department: "Sales",
-                //             StartDate: "2023-08-01T00:00:00Z",
-                //             Role: "Manager",
-                //             Address: "123 Main St",
-                //             CityState: "Anytown, USA",
-                //             ZipCode: "12345",
-                //             Email: "john.doe@example.com",
-                //             PhoneNumber: "555-123-4567",
-                //             Photo: "url/to/photo.jpg"
-                //         },
-                //         {
-                //             Id: "1002",
-                //             Name: "Jane Smith",
-                //             Department: "Marketing",
-                //             StartDate: "2023-08-01T00:00:00Z",
-                //             Role: "Marketing Specialist",
-                //             Address: "456 Oak Ave",
-                //             CityState: "Anothercity, USA",
-                //             ZipCode: "67890",
-                //             Email: "jane.smith@example.com",
-                //             PhoneNumber: "555-987-6543",
-                //             Photo: "url/to/photo2.jpg"
-                //         },
-                //         {
-                //             Id: "1003",
-                //             Name: "Bob Johnson",
-                //             Department: "IT",
-                //             StartDate: "2023-08-01T00:00:00Z",
-                //             Role: "Software Engineer",
-                //             Address: "789 Elm St",
-                //             CityState: "Techcity, USA",
-                //             ZipCode: "54321",
-                //             Email: "bob.johnson@example.com",
-                //             PhoneNumber: "555-555-5555",
-                //             Photo: "url/to/photo3.jpg"
-                //         }
-                //     ]
-                // };
-
                 // var oModel = new JSONModel(oData);
                 // this.getView().setModel(oModel);
             },
@@ -70,8 +26,8 @@ sap.ui.define([
                 this._showObject(oEvent.getSource());
             },
             _showObject: function (oItem) {
-                this.getRouter().navTo("object", {
-                    objectId: oItem.getBindingContext().getPath().substring("/SkillSet".length)
+                this.getRouter().navTo("Detail", {
+                    employeeId: oItem.getBindingContext().getPath().substring("/Employee".length)
                 });
             },
 
