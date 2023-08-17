ffc63b22d74b155d537dff1d39a4a83f40660c81
Juliette Lelchuk <juliettelelchuk@mindsetconsulting.com>
2023-08-17 17:39:56 -0500

    Enable editing of individual Skills

--- adt_ui5/webapp/controller/SkillList.controller.js
+++ adt_ui5/webapp/controller/SkillList.controller.js
@@ -10,14 +10,40 @@ sap.ui.define([
         onInit: function () {
             var oModel = new JSONModel({
                 Skills: [
-                    { skillName: "Skill 1" },
-                    { skillName: "Skill 2" },
-                    { skillName: "Skill 3" }
+                    { skillName: "Skill 1", id: 1, editMode: false },
+                    { skillName: "Skill 2", id: 2, editMode: false },
+                    { skillName: "Skill 3", id: 3, editMode: false }
                 ]
             });
 
             this.getView().setModel(oModel);
 
+        },
+
+        onAddSkillPress: function () {
+            var oModel = this.getView().getModel();
+            var aSkills = oModel.getProperty("/Skills");
+
+            // Add a new empty skill to the model with an editMode property
+            aSkills.push({ skillName: "", editMode: true });
+            oModel.setProperty("/Skills", aSkills);
+        },
+
+        onEditSkill: function (oEvent) {
+            var oModel = this.getView().getModel();
+            var oContext = oEvent.getSource().getBindingContext();
+            oModel.setProperty(oContext.getPath() + "/editMode", true);
+        },
+
+        onSaveSkillPress: function (oEvent) {
+            var oModel = this.getView().getModel();
+            var oContext = oEvent.getSource().getBindingContext();
+            var sSkillPath = oContext.getPath();
+            var sSkillName = oModel.getProperty(sSkillPath + "/skillName").trim();
+
+            // Save the edited skill name and set back to view mode
+            oModel.setProperty(sSkillPath + "/skillName", sSkillName);
+            oModel.setProperty(sSkillPath + "/editMode", false);
         },
 
         getRouter: function () {
